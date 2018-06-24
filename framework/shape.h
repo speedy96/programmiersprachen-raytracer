cce75473ce0a38729fc2b14ac08f537830e58316
speedy96 <lennardgabriel@gmx.de>
2018-06-24 09:54:32 +0200

    aufgabe 5.1 (shape.h) added

---
+++ framework/shape.h
@@ -0,0 +1,14 @@
+//
+// Created by SYSTEM on 24.06.2018.
+//
+
+#ifndef PROJECT_SHAPE_H
+#define PROJECT_SHAPE_H
+
+class Shape{
+public:
+    virtual double area()const = 0;
+    virtual float volume()const = 0;
+};
+
+#endif //PROJECT_SHAPE_H
